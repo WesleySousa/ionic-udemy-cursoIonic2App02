8720229c5e1781664c5c50a412fcbdf63ac619de
Wesley Sousa Silva <wesley.sousa59@gmail.com>
2018-01-10 23:26:03 -0300

    aula de navegação entre páginas

---
+++ src/pages/contact/contact.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html',
+})
+export class ContactPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ContactPage');
+  }
+
+  pushPage(): void {
+    this.navCtrl.push(ContactPage);
+  }
+
+  popPage(): void {
+    this.navCtrl.pop();
+  }
+
+}
